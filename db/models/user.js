6a35a09ba1c7b1801b88de84ce302ca5eac1801c
Ben Pu <benpu@Bens-iMac.local>
2020-09-26 22:52:26 -0400

    setting up user models

--- db/models/user.js
+++ db/models/user.js
@@ -1,4 +1,7 @@
 'use strict';
+
+const { default: User } = require("../../client/src/components/User");
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     email: {
@@ -13,7 +16,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       type: DataTypes.STRING,
       validates: {
-        len: [1, 255],
+        len: [1, 40],
       },
     },
     hashedPassword: {
@@ -31,5 +34,46 @@ module.exports = (sequelize, DataTypes) => {
   User.associate = function(models) {
   };
 
+ 
+
+
+  User.prototype.toSafeObject = function () {
+    const {
+      id,
+      username
+    } = this;
+
+    return { id, username};
+  }
+
+  User.prototype.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.hashedPassword.toString());
+  };
+
+  User.getCurrentUserById = async function(id) {
+    return await User.scope("currentuser").findByPk(id);
+  }
+
+  User.login = async function ({username, password}) {
+    const user = await User.scope('loginuser').findOne({
+      where: {
+        [Op.or]: [{username}, {email: username}],
+      }
+    })
+
+    if (user && user.validatePassword(password)) {
+      return await User.getCurrentUserById(user.id);
+    }
+  }
+
+  User.signup = async function({ username, email, password }) {
+    const hashedPassword = bcrypt.hashsync(password);
+    const user = await User.create({
+      username,
+      email,
+      hashedPassword
+    });
+    return await User.getCurrentUserById(user.id);
+  }
   return User;
 };
