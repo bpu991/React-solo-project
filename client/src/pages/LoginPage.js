f80b978944eaebdb267d349f4826030849b7f9ff
Ben Pu <benpu@Bens-iMac.local>
2020-09-28 19:48:06 -0400

    fixing bugs

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,122 +1,126 @@
+import React, { useState } from 'react';
+import { login } from '../store/auth';
+import { useSelector, useDispatch } from 'react-redux'
+import { Redirect } from 'react-router-dom';
+
+const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const isLoggedIn = useSelector(state => !!state.auth.id)
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+       dispatch(login(username, password));
+    };
+    console.log(isLoggedIn)
+    if(isLoggedIn) return <Redirect to='/'/>
+
+    return (
+        <form>
+            <label>
+                Username
+                <input 
+                    type='text' 
+                    name='username' 
+                    value={username}
+                    onChange={(e)=> setUsername(e.target.value)}
+                />
+            </label>
+            <label>
+                Password
+                <input
+                    type='password'
+                    name='password'
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </label>
+            <button onClick={handleSubmit} type='submit'>Log in</button>
+        </form>
+    )
+}
+
+export default LoginPage;
+
 // import React, { useState } from 'react';
+// import { Redirect } from 'react-router-dom';
+// import Cookies from 'js-cookie';
 // import { useDispatch } from 'react-redux';
-// import { login } from '../store/auth';
 
-// const LoginPage = () => {
-//     const [username, setUsername] = useState('');
-//     const [password, setPassword] = useState('');
-//     const dispatch = useDispatch();
+// const SET_USER = 'auth/SET_USER'
 
-//     const handleSubmit = (e) => {
+// const setUser = (user) => {
+//     return {
+//         type: SET_USER,
+//         user
+//     };
+// };
+// function LoginPanel({ updateUser, currentUserId }) {
+//     const [username, setUsername] = useState('demo-lition');
+//     const [password, setPassword] = useState('password');
+//     const dispatch = useDispatch()
+
+//     const handleSubmit = async (e) => {
 //         e.preventDefault();
-//         dispatch(login(username, password));
+//         const res = await fetch('/api/session', {
+//             method: 'put',
+//             headers: {
+//                 "Content-Type": 'application/json',
+//                 "XSRF-TOKEN": Cookies.get('XSRF-TOKEN')
+//             },
+//             body: JSON.stringify({ username, password })
+//         });
+//         res.data = await res.json();
+//         if (res.ok) {
+//             dispatch(setUser(res.data));
+//         }
+//         return res;
 //     };
 
+//     const updateUsername = e => {
+//         setUsername(e.target.value);
+//     };
+
+//     const updatePassword = e => {
+//         setPassword(e.target.value);
+//     };
+
+//     if (currentUserId) {
+//         return <Redirect to="/" />;
+//     }
 //     return (
-//         <form onSubmit={handleSubmit}>
-//             <label>
-//                 Username
-//                 <input 
-//                     type='text' 
-//                     name='username' 
+//         <main className="centered middled">
+//             <form onSubmit={handleSubmit}>
+//                 <input type="text"
+//                     placeholder="Email"
 //                     value={username}
-//                     onChange={(e)=> setUsername(e.target.value)}
-//                 />
-//             </label>
-//             <label>
-//                 Password
-//                 <input
-//                     type='password'
-//                     name='password'
-//                     value={password} 
-//                     onChange={(e) => setPassword(e.target.value)}
-//                 />
-//             </label>
-//             <button type='submit'>Log in</button>
-//         </form>
-//     )
+//                     onChange={updateUsername} />
+//                 <input type="password"
+//                     placeholder="Password"
+//                     value={password}
+//                     onChange={updatePassword} />
+//                 <button type="submit">Login</button>
+//             </form>
+//         </main>
+//     );
 // }
+// export default LoginPanel;
 
-// export default LoginPage;
-
-import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
-import Cookies from 'js-cookie';
-import { useDispatch } from 'react-redux';
-
-const SET_USER = 'auth/SET_USER'
-
-const setUser = (user) => {
-    return {
-        type: SET_USER,
-        user
-    };
-};
-function LoginPanel({ updateUser, currentUserId }) {
-    const [username, setUsername] = useState('demo-lition');
-    const [password, setPassword] = useState('password');
-    const dispatch = useDispatch()
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const res = await fetch('/api/session', {
-            method: 'put',
-            headers: {
-                "Content-Type": 'application/json',
-                "XSRF-TOKEN": Cookies.get('XSRF-TOKEN')
-            },
-            body: JSON.stringify({ username, password })
-        });
-        res.data = await res.json();
-        if (res.ok) {
-            dispatch(setUser(res.data));
-        }
-        return res;
-    };
-
-    const updateUsername = e => {
-        setUsername(e.target.value);
-    };
-
-    const updatePassword = e => {
-        setPassword(e.target.value);
-    };
-
-    if (currentUserId) {
-        return <Redirect to="/" />;
-    }
-    return (
-        <main className="centered middled">
-            <form onSubmit={handleSubmit}>
-                <input type="text"
-                    placeholder="Email"
-                    value={username}
-                    onChange={updateUsername} />
-                <input type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={updatePassword} />
-                <button type="submit">Login</button>
-            </form>
-        </main>
-    );
-}
-export default LoginPanel;
-
-const login = (username, password) => {
-    return async (dispatch) => {
-        const res = await fetch('/api/session', {
-            method: 'put',
-            headers: {
-                "Content-Type": 'application/json',
-                "XSRF-TOKEN": Cookies.get('XSRF-TOKEN')
-            },
-            body: JSON.stringify({ username, password })
-        });
-        res.data = await res.json();
-        if (res.ok) {
-            dispatch(setUser(res.data));
-        }
-        // return res;
-    };
-};
+// const login = (username, password) => {
+//     return async (dispatch) => {
+//         const res = await fetch('/api/session', {
+//             method: 'put',
+//             headers: {
+//                 "Content-Type": 'application/json',
+//                 "XSRF-TOKEN": Cookies.get('XSRF-TOKEN')
+//             },
+//             body: JSON.stringify({ username, password })
+//         });
+//         res.data = await res.json();
+//         if (res.ok) {
+//             dispatch(setUser(res.data));
+//         }
+//         // return res;
+//     };
+// };
---
+++ client/src/pages/Signup.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { signup } from '../store/auth';
+
+
+
+const SignUpPage = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [userName, setUserName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const dispatch = useDispatch()
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(signup(firstName, lastName, userName, email, password, confirmPassword));
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                First Name
+                <input type="text"
+                    placeholder="First Name"
+                    value={firstName}
+                    onChange={e => setFirstName(e.target.value)} />
+            </label>
+            <label>
+                Last Name
+                <input type="text"
+                    placeholder="Last Name"
+                    value={lastName}
+                    onChange={e => setLastName(e.target.value)} />
+            </label>
+            <label>
+                User Name
+                <input type="text"
+                    placeholder="User Name"
+                    value={userName}
+                    onChange={e => setUserName(e.target.value)} />
+            </label>
+            <label>
+                Email
+                <input type="text"
+                    placeholder="Email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)} />
+            </label>
+            <label>
+                Password
+                <input type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)} />
+            </label>
+            <label>
+                Confirm Password
+                <input type="password"
+                    placeholder="Confirm Password"
+                    value={confirmPassword}
+                    onChange={e => setConfirmPassword(e.target.value)} />
+            </label>
+            <button type="submit">Sign-up</button>
+        </form>
+    );
+}
+
+export default SignUpPage
