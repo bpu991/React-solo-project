a2c128c58257439643e03af6ba668c5ac9ef3bdc
Ben Pu <bpu991@gmail.com>
2020-12-18 16:27:25 -0500

    fixed like feature

--- client/src/components/SinglePhoto.js
+++ client/src/components/SinglePhoto.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useLocalStorage } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { useParams, useHistory, NavLink } from 'react-router-dom';
 import { getSinglePhoto, likePost, unlikePost} from '../actions/photo_actions';
@@ -38,7 +38,9 @@ const SinglePhoto = () => {
     const comments = useSelector(state => state.commentReducer.comments)
     const currentUser = useSelector(state => state.authReducer)
     const [content, setContent] = useState("");
-    const [liked, setLiked] = useState(false)
+    // const [liked, setLiked] = useState(false)
+    const [liked, setLiked] = useLocalStorage('liked', false)
+
     const params = useParams();
     const dispatch = useDispatch()
     const history = useHistory()
@@ -76,6 +78,42 @@ const SinglePhoto = () => {
     function goRight() {
         history.push(`/photos/${photo.id + 1}`)
         window.location.reload(false);
+    }
+
+    function useLocalStorage(key, initialValue) {
+        // State to store our value
+        // Pass initial state function to useState so logic is only executed once
+        const [storedValue, setStoredValue] = useState(() => {
+            try {
+                // Get from local storage by key
+                const item = window.localStorage.getItem(key);
+                // Parse stored json or if none return initialValue
+                return item ? JSON.parse(item) : initialValue;
+            } catch (error) {
+                // If error also return initialValue
+                console.log(error);
+                return initialValue;
+            }
+        });
+
+        // Return a wrapped version of useState's setter function that ...
+        // ... persists the new value to localStorage.
+        const setValue = value => {
+            try {
+                // Allow value to be a function so we have same API as useState
+                const valueToStore =
+                    value instanceof Function ? value(storedValue) : value;
+                // Save state
+                setStoredValue(valueToStore);
+                // Save to local storage
+                window.localStorage.setItem(key, JSON.stringify(valueToStore));
+            } catch (error) {
+                // A more advanced implementation would handle the error case
+                console.log(error);
+            }
+        };
+
+        return [storedValue, setValue];
     }
     return (
         <>  
